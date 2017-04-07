334a96b003f74f39b2db3ff81db9e22f26bae272
Matt Oates <matt@boardom.io>
2017-04-06 17:12:49 -0700

    better error handling

--- src/WebIndex.js
+++ src/WebIndex.js
@@ -1,7 +1,10 @@
 import Stab from './Stab';
 
-// TODO: better error handling
 const el = document.getElementById('ab-testing-json');
-const groups = JSON.parse(el.innerHTML);
+let groups = {};
+
+if (el) {
+    groups = JSON.parse(el.innerHTML);
+}
 
 export default new Stab(groups.abTests);
