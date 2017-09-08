34d8f7dfca6c533735e96391e04bc2913030892c
Matt Oates <matt@boardom.io>
2017-09-08 08:18:11 -0700

    Remove warnings

--- src/WebIndex.js
+++ src/WebIndex.js
@@ -8,16 +8,12 @@ let groups = {};
 
 if (typeof __SITEVARS__ !== 'undefined' && __SITEVARS__.abTests) {
   groups = __SITEVARS__.abTests;
-} else {
-  error('Test groups not found');
 }
 
 const stab = new Stab(groups);
 
 if (typeof mixpanel !== 'undefined') {
   mixpanel.register_once(stab.getGroups());
-} else {
-  error('Mixpanel not found');
 }
 
 export default stab;
